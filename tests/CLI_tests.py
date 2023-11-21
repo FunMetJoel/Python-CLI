bf9d0b6e2924a176b0b8c91987a4b53cb3c859d4
FunMetJoel <101552207+FunMetJoel@users.noreply.github.com>
2023-11-21 20:16:40 +0100

    Removed tests: not working

--- tests/CLI_tests.py
+++ tests/CLI_tests.py
@@ -4,17 +4,12 @@ from custCLI import CLI, Command, Parameter, DataType
 
 class CommandTestCase(unittest.TestCase):
 
-    def testFunction(params):
-        print("testFunction")
-        for param in params:
-            print(f"\t{param}")
-
-    def setUp(self):
-        self.multiplication = Command("test1", "test1 description", self.testFunction, [Parameter("param1", "param1 description", DataType.STRING), Parameter("param2", "param2 description", DataType.INTEGER)])
-
     def test_test(self):
         """Test 0 multiplied by 2"""
 
         # 0 multiplied by 2 return 0
-        result = (0 * 2)
+        result = int(0 * 2)
         self.assertEqual(result, 0)
+
+if __name__ == '__main__':
+    unittest.main()
